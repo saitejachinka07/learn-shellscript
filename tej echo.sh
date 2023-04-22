8cb400fef121d9809d62575b41a96f9629f91cf8
saitej <chinka.saiteja@gmail.com>
2023-04-22 11:44:26 +0530

    color command

--- tej echo.sh
+++ tej echo.sh
@@ -1,1 +1,12 @@
 echo hello world
+#enable the color
+#syntax
+# * echo -e "\e[COLmMESSAGE\e[0m"
+# "-e " is tho enable \e 
+# all the input should be in there i  quotes
+# \e is to enable a color
+# COLm is the color code
+# Red(31m),Green(32m),yellow(33m),Blue(34m),magenta(35m),Cyan(36m)
+
+echo -e "\e[36mcCYAN COLOR"
+echo hii all how are you
